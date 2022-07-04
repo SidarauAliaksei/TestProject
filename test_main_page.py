c025cd323232b65e22b06b48988efdb132d2686f
Aliaksei Sidarau <www.bart@bk.ru>
2022-07-04 17:48:19 +0300

    added function checks login and register

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,5 @@
-from selenium.webdriver.common.by import By
-
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -13,3 +7,26 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_guest_should_see_login_field(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page2 = LoginPage(browser, link)
+    page2.open()
+    page2.should_be_login_form()
+
+
+def test_guest_should_see_register_field(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
